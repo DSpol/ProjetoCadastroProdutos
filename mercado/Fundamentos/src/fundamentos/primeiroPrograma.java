7952afe5e106bb39f76489336721c25661c8c231
DSpol <104375735+DSpol@users.noreply.github.com>
2022-04-25 12:41:14 -0300

    Delete mercado/Fundamentos directory
    não necessário

--- mercado/Fundamentos/src/fundamentos/primeiroPrograma.java
+++
@@ -1,9 +0,0 @@
-package fundamentos;
-
-public class primeiroPrograma {
-
-    public static void main(String[] args) {
-        System.out.println("Primeiro programa parte 1!");
-        System.out.println("Primeiro programa parte 2!");
-    }
-}
